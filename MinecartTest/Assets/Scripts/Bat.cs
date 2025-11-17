60e3835488bb885decbbc7d097a608f92b3e4180
Gabriel G. Cunha <gabrielmesmo97@gmail.com>
2025-11-17 11:01:35 -0300

    Add prototype

---
+++ MinecartTest/Assets/Scripts/Bat.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Bat : MonoBehaviour
+{
+    [SerializeField] private GameObject _minecart;
+    private float _moveSpeed = 2f;
+
+    private Rigidbody2D _rb;
+    private bool _isHovered;
+
+    private const float _MAX_DISTANCE_TO_MINECART = 0.8f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        Vector2 direction = (_minecart.transform.position - transform.position).normalized;
+        float distance = Vector3.Distance(_minecart.transform.position, transform.position);
+
+        if (distance >= _MAX_DISTANCE_TO_MINECART)
+        {
+            Move(direction);
+        }
+
+        if (_isHovered && Mouse.current.leftButton.wasPressedThisFrame)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void Move(Vector2 direction)
+    {
+        Vector2 myPosition = _rb.position;
+        myPosition += _moveSpeed * direction * Time.deltaTime;
+
+        _rb.MovePosition(myPosition);
+    }
+
+    private void OnMouseEnter()
+    {
+        _isHovered = true;
+    }
+
+    private void OnMouseExit()
+    {
+        _isHovered = false;
+    }
+}
---
+++ MinecartTest/Assets/Scripts/Minecart.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+
+public class Minecart : MonoBehaviour
+{
+    [SerializeField] private GameObject _endGoal;
+    private float _moveSpeed = 3f;
+    private bool _canMove = true;
+
+    private Rigidbody2D _rb;
+
+    private const float _MIN_DISTANCE_TO_END_GOAL = 0.2f;
+
+    private void Awake()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void Start()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if (_canMove)
+        {
+            Move(Vector2.right);
+        }
+
+        if (Vector3.Distance(transform.position, _endGoal.transform.position) <= _MIN_DISTANCE_TO_END_GOAL)
+        {
+            _canMove = false;
+            transform.position = _endGoal.transform.position;
+        }
+    }
+
+    private void Move(Vector2 direction)
+    {
+        Vector2 myPosition = _rb.position;
+        myPosition += _moveSpeed * direction * Time.deltaTime;
+
+        _rb.MovePosition(myPosition);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _canMove = false;
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        _canMove = true;
+    }
+}
